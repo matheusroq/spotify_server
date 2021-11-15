06fd6450db991a7dd40092654ef33beedab8ecbc
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-15 10:01:46 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import { router } from './routes';
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(router);
 
 export { app };

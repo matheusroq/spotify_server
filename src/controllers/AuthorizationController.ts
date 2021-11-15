8c1b203186d357c3e98f5b5ca774f665f6b8a5a9
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-15 10:02:01 -0300

    add login method

--- src/controllers/AuthorizationController.ts
+++ src/controllers/AuthorizationController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from 'express';
 import { AuthorizationService } from '../services/AuthorizationService';
 
 class AuthorizationController {
+  async login(request: Request, response: Response) {
+    const scope = [
+      'streaming',
+      'user-read-email',
+      'user-read-private',
+      'user-library-read',
+      'user-library-modify',
+      'user-read-playback-state',
+      'user-modify-playback-state',
+    ].reduce((ac, scope, index) => {
+      if (index === 0) return `${scope}${ac}`;
+      return `${ac}%20${scope}`;
+    }, '');
+
+    const url = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=${scope}`;
+    return response.redirect(url);
+  }
   async getAccess(request: Request, response: Response) {
     const { code } = request.body;
     const authorizationService = new AuthorizationService();

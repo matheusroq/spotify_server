9ae510c97c45437476853e6aa53befb6ccfd0ebc
Matheus Roque <matheusfariasroq@gmail.com>
2021-11-20 16:25:09 -0300

    feat: add search route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { AuthorizationController } from './controllers/AuthorizationController';
+import { SearchController } from './controllers/SearchController';
 
 const router = Router();
-
+router.get('/', (req, res) => res.redirect('/login'));
+//controllers
 const auhtorizationController = new AuthorizationController();
+const searchController = new SearchController();
 
 router.get('/login', auhtorizationController.login);
 router.post('/auth', auhtorizationController.getAccess);
+
+// search
+router.get('/search', searchController.getSearch);
 
 export { router };
